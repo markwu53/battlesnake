08f252791f029956e7ed73133b829e352946fc99
Mark Wu <mark.wu@encova.com>
2025-05-11 19:42:14 -0400

    fix target

--- src/battlesnakes/snake.py
+++ src/battlesnakes/snake.py
@@ -225,7 +225,7 @@ def move(game_state: typing.Dict) -> typing.Dict:
         food_dist = [abs(f["x"]-my_head["x"]+abs(f["y"]-my_head["y"])) for f in foods]
         min_dist = 100 if len(food_dist) == 0 else min(food_dist)
 
-        return (food_density, min_dist)
+        return (food_density, 100-min_dist)
 
     def calc_target_area():
 
