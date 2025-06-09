f1e4efe56b737daf66517726601bd08cfcfa17d0
Mark Wu <mark.wu@encova.com>
2025-06-09 17:24:55 -0400

    try kill

--- src/battlesnakes/snake.py
+++ src/battlesnakes/snake.py
@@ -428,13 +428,15 @@ def move(game_state: typing.Dict) -> typing.Dict:
             if game_state["me"]["health"] >= 50:
                 my_body = game_state["me"]["body"]
                 snake_body = game_state["others"][0]["body"]
-                if len(my_body) >= len(snake_body)+3:
-                    if path_distance_pq(my_body[0], snake_body[0]) <= 6:
-                        if any([on_border(p) for p in adj_cells(snake_body[0])]):
-                            moves = shortest_path_move(my_body[0], snake_body[0])
-                            moves = [move for move in moves if move in game_state["allowed_move"]]
-                            if len(moves) > 0:
-                                game_state["next_head_coord"] = moves[0]
+                if (1==1
+                    and len(my_body) >= len(snake_body)+3
+                    #and path_distance_pq(my_body[0], snake_body[0]) <= 6
+                    #and any([on_border(p) for p in adj_cells(snake_body[0])])
+                ):
+                    moves = shortest_path_move(my_body[0], snake_body[0])
+                    moves = [move for move in moves if move in game_state["allowed_move"]]
+                    if len(moves) > 0:
+                        game_state["next_head_coord"] = moves[0]
 
 
     def best_choice_avoid_danger():
