12f0c10497f416594b33545f1f2d27ffd187d081
Mark Wu <mark.wu@encova.com>
2025-06-12 11:37:52 -0400

    import and test

--- tests/test_snake.py
+++ tests/test_snake.py
@@ -1,11 +1,5 @@
-from battlesnakes.snake import info
+#from battlesnakes.snake import info
 #import pytest
-
-
-def test_info() -> None:
-    result = info()
-    assert result["apiversion"] == "1"
-
 
 def name_coordinate_test_cases(value):
     if isinstance(value, dict):
