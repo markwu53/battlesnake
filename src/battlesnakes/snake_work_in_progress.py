a95bf475124b60e1016b2e2d4126bcc34b3c3316
Mark Wu <mark.wu@encova.com>
2025-06-13 18:07:09 -0400

    sensitive condition

--- src/battlesnakes/snake_work_in_progress.py
+++ src/battlesnakes/snake_work_in_progress.py
@@ -677,7 +677,7 @@ def special_experimenting_code(game_state):
             def sensitive_to_enemy_move(a):
                 cn = game_state["danger_ranking"][a]["dead_end"]
                 #a is sensitive
-                return any([x/cn <= 0.7 and x+5 <= cn 
+                return any([(x/cn <= 0.7 and x+5 <= cn) or x <= 6
                         for path in snake_paths
                         for x in [len(path_connected(a, occupied+list(path)))]
                         ])
