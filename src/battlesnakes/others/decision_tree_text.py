aff7eb120ac55ed26dba1e6ed9641d40430a3f09
Mark Wu <mark.wu@encova.com>
2025-06-20 16:09:59 -0400

    fix

--- src/battlesnakes/others/decision_tree_text.py
+++ src/battlesnakes/others/decision_tree_text.py
@@ -7,17 +7,17 @@ BEGIN = env_1_vs_1?
             no_danger+ = food
                 food = env_food_near?
                     env_food_near+ = get_food
-                        get_food = TODO
+                        get_food = routine_move!
                     env_food_near- = chase_tail
                         chase_tail = see_my_tail?
                             see_my_tail+ = chase_my_tail
-                                chase_my_tail = TODO
+                                chase_my_tail = routine_move!
                             see_my_tail- = see_enemy_tail?
                                 see_enemy_tail+ = chase_enemy_tail
-                                    chase_enemy_tail = TODO
+                                    chase_enemy_tail = routine_move!
                                 see_enemy_tail- = no_tail
-                                    no_tail = TODO
-            no_danger- = TODO
+                                    no_tail = routine_move!
+            no_danger- = routine_move!
         env_mine_bigger- = env_less_than_8?
             env_less_than_8+ = env_lt8_smaller?
                 env_lt8_smaller+ = env_lt8_enemy_far?
@@ -32,7 +32,7 @@ BEGIN = env_1_vs_1?
                 env_lt8_smaller- = env_food_near?
             env_less_than_8- = env_lt8_smaller?
     env_1_vs_1- = env_1_vs_n
-        env_1_vs_n = TODO
+        env_1_vs_n = routine_move!
 
 """
 
