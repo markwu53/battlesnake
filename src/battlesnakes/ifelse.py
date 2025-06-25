3a4c6a6e02deec78350c0b27e0d58931ee7e1c0c
Mark Wu <mark.wu@encova.com>
2025-06-25 18:54:49 -0400

    remove log for localtime

--- src/battlesnakes/ifelse.py
+++ src/battlesnakes/ifelse.py
@@ -617,7 +617,7 @@ def special_experimenting_code(game_state):
 
     g.log["experiment"] = True
     g.start_time = time.time()
-    g.e.localtime = time.localtime()
+    #g.e.localtime = time.localtime()
 
     decision()
     g.state["next_move"] = get_adjacent_dir(g.s.my_head, g.next_coord)
