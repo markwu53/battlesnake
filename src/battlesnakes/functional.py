13e03ef91a7434ed04e39f8c636176600ccbaa2f
Mark Wu <mark.wu@encova.com>
2025-06-27 18:01:06 -0400

    update

--- src/battlesnakes/functional.py
+++ src/battlesnakes/functional.py
@@ -598,10 +598,12 @@ def shorter_by_1(moves):
     if g.s.my_length+1 == g.s.other_length:
         if off_border_1(g.s.my_head):
             if any([a in g.food for a in moves]):
+                g.decision_path.append("get the food and length will be equal")
                 return moves
 
 def near_border(moves):
     if off_border_1(g.s.my_head):
+        g.decision_path.appened("avoid border")
         return prefer_no(on_border)(moves)
 
 def head_distance_4(moves):
