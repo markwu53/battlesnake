c9bddbcc25470dac96d5f5e34bf903ab6add3208
Mark Wu <mark.wu@encova.com>
2025-06-27 16:32:37 -0400

    fix

--- src/battlesnakes/functional.py
+++ src/battlesnakes/functional.py
@@ -612,6 +612,7 @@ def equal_length_danger(moves):
     if g.e.head_distance == 2:
         collision_points = [a for a in adj_cells(g.s.my_head) if is_adjacent(a, g.s.other_head) and a not in g.occupied_cells[0]]
         return prefer_no(lambda a: a in collision_points)(moves)
+    return moves
 
 def equal_length(moves):
     if g.s.my_length == g.s.other_length:
