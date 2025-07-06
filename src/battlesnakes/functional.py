a0686c4413be6c18037502a0dfc2f7686bac6285
Mark Wu <mark.wu@encova.com>
2025-07-06 16:41:50 -0400

    remove wayout calculation

--- src/battlesnakes/functional.py
+++ src/battlesnakes/functional.py
@@ -879,7 +879,7 @@ def chase_my_tail_my_snake_longer(moves):
                     if len(food_tail_connect) != 0:
                         g.decision_path.append("detour get food1")
                         return food_tail_connect
-                    return tail_moves
+                    #return tail_moves
                     food4 = [f for f in g.food if distance_pq(f, g.s.my_head) <= 4]
                     if len(food4) == 0:
                         return tail_moves
@@ -997,7 +997,7 @@ def too_long(moves):
     if g.s.my_length >= 20:
         moves = sequential([
             split_choice,
-            wayout,
+            #wayout,
             chase_tail,
             prefer_more_next_move,
             prefer_middle_by_3,
