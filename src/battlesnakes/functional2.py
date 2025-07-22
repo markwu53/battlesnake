aee0657986cb4f90bf3add5d3fa582f50fc8fe1e
Mark Wu <mark.wu@encova.com>
2025-07-22 12:01:28 -0400

    update 1v1 wayout to meander

--- src/battlesnakes/functional2.py
+++ src/battlesnakes/functional2.py
@@ -346,6 +346,7 @@ def experiment_condition():
     # return False
     if len(g.others) > 1: return True
     if len(g.others) == 1 and g.me.length <= 10: return True
+    if len(g.others) == 0: return True
     return False
 
 def special_experimenting_code(game_state):
@@ -424,6 +425,11 @@ def decision():
     
     if len(g.e.allowed_moves) == 1:
         #no choice
+        g.next_coord = g.e.allowed_moves[0]
+        return
+
+    if len(g.others) == 0:
+        #win
         g.next_coord = g.e.allowed_moves[0]
         return
 
@@ -1028,6 +1034,8 @@ def run():
     log = {'id': '0d7da7b2-c47e-435c-b80d-f48b7e5f66bc', 'turn': 115, 'me': {'name': 'mark_snake', 'health': 96, 'body': [(0, 7), (0, 6), (0, 5), (0, 4), (0, 3), (1, 3), (1, 4), (1, 5), (1, 6), (2, 6), (2, 5), (2, 4), (2, 3)]}, 'others': [{'name': 'Würmchen', 'health': 15, 'body': [(8, 1), (8, 2), (8, 3), (7, 3), (6, 3), (6, 2)]}, {'name': 'snakey_wakey', 'health': 91, 'body': [(3, 4), (3, 5), (3, 6), (3, 7), (4, 7), (4, 6), (5, 6), (5, 5), (6, 5), (7, 5), (8, 5), (9, 5)]}, {'name': 'Wim HU', 'health': 84, 'body': [(3, 8), (4, 8), (4, 9), (5, 9), (6, 9), (6, 8), (6, 7), (7, 7), (8, 7), (8, 6), (9, 6), (9, 7)]}], 'food': [(7, 2), (4, 2)], 'module': 'functional2', 'decision_path': [], 'next_coord': (1, 7), 'next_move': 'right', 'time': '0.002s'}
     log = {'id': '3c112738-4775-4965-9d95-b7a53108fa6e', 'turn': 29, 'me': {'name': 'mark_snake', 'health': 79, 'body': [(7, 6), (7, 5), (7, 4), (7, 3), (7, 2)]}, 'others': [{'name': 'FerralSnake-standard', 'health': 97, 'body': [(5, 8), (6, 8), (6, 9), (6, 10), (5, 10)]}, {'name': 'Wim HU', 'health': 83, 'body': [(3, 4), (3, 3), (3, 2), (3, 1), (3, 0)]}, {'name': 'suboptimal', 'health': 96, 'body': [(7, 10), (8, 10), (9, 10), (10, 10), (10, 9), (9, 9), (9, 8), (9, 7)]}], 'food': [(4, 4), (5, 7), (3, 9)], 'module': 'functional2', 'decision_path': ['go to open space (5, 5)'], 'next_coord': (6, 6), 'next_move': 'left', 'time': '0.068s'}
     log = {'id': '7a0129ad-a8ea-427f-869c-a8281d54fcec', 'turn': 153, 'me': {'name': 'mark_snake', 'health': 89, 'body': [(4, 9), (4, 10), (5, 10), (6, 10), (6, 9), (7, 9), (7, 8), (6, 8), (5, 8), (5, 7), (6, 7), (7, 7), (7, 6), (6, 6), (5, 6), (4, 6)]}, 'others': [{'name': 'FerralSnake-standard', 'health': 73, 'body': [(2, 3), (2, 4), (3, 4), (3, 3), (4, 3), (4, 4), (5, 4), (5, 3), (5, 2)]}, {'name': 'rustiger', 'health': 82, 'body': [(10, 7), (10, 8), (10, 9), (10, 10), (9, 10), (8, 10), (8, 9), (9, 9), (9, 8), (9, 7), (9, 6), (9, 5), (8, 5), (7, 5)]}, {'name': 'Snakeformatika', 'health': 96, 'body': [(2, 7), (2, 6), (3, 6), (3, 7), (3, 8), (3, 9), (3, 10), (2, 10), (1, 10)]}], 'food': [(8, 6), (9, 2)], 'module': 'functional2', 'decision_path': ["don't go in trap [(4, 8)]"], 'next_coord': (5, 9), 'next_move': 'right', 'time': '0.000s'}
+    log = {'id': '27f2baaf-30e2-4314-8173-b5fbe5cc403b', 'turn': 52, 'me': {'name': 'mark_snake', 'health': 52, 'body': [(9, 7), (10, 7), (10, 8), (9, 8)]}, 'others': [{'name': 'snakey_wakey', 'health': 90, 'body': [(1, 7), (2, 7), (3, 7), (3, 6), (4, 6), (4, 5), (4, 4), (4, 3), (5, 3), (6, 3)]}, {'name': 'Snakeformatika', 'health': 92, 'body': [(0, 6), (0, 7), (0, 8), (0, 9), (0, 10), (1, 10)]}, {'name': 'Hovering Hobbs', 'health': 88, 'body': [(7, 5), (7, 6), (7, 7), (7, 8), (6, 8), (5, 8)]}], 'food': [(6, 1), (0, 3), (10, 6)], 'module': 'functional2', 'decision_path': ['killer near', 'go to food'], 'next_coord': (9, 6), 'next_move': 'down', 'time': '0.010s'}
+    log = {'id': '06c3573a-c6b9-45c2-b5f0-8505719d89fc', 'turn': 130, 'me': {'name': 'mark_snake', 'health': 93, 'body': [(9, 1), (8, 1), (7, 1), (6, 1), (5, 1), (5, 0), (4, 0), (3, 0), (3, 1), (2, 1)]}, 'others': [{'name': 'snakey_wakey', 'health': 59, 'body': [(7, 3), (6, 3), (5, 3), (4, 3), (4, 4), (4, 5), (4, 6), (5, 6), (6, 6), (7, 6), (8, 6), (9, 6), (10, 6)]}, {'name': 'rustiger', 'health': 96, 'body': [(1, 3), (1, 2), (2, 2), (3, 2), (3, 3), (3, 4), (3, 5), (3, 6), (3, 7), (3, 8)]}], 'food': [(5, 10), (8, 0)], 'module': 'functional2', 'decision_path': ['killer near', 'go to food'], 'next_coord': (9, 0), 'next_move': 'down', 'time': '0.008s'}
 
     game_state = init_from_log(log)
     special_experimenting_code(game_state)
