c3a1bba08999ab65415d85b9e0af246c01e0246b
Mark Wu <mark.wu@encova.com>
2025-07-26 19:52:14 -0400

    update

--- src/battlesnakes/simp_entry.py
+++ src/battlesnakes/simp_entry.py
@@ -380,7 +380,7 @@ def main(game_state):
         moves = sequential([
             kill_oppotunities,
             avoid_danger,
-            split_choice,
+            (split_choice),
             wayout,
             get_food,
             other_considerations,
@@ -535,7 +535,7 @@ def main(game_state):
         return cases([
             split_1vn_short,
             split_1vn_medium,
-            split_1vn_long,
+            (split_1vn_long),
         ])(moves)
 
     def split_1vn_short(moves):
@@ -555,17 +555,17 @@ def main(game_state):
             return
         g.decision_path.append("split long")
         return sequential([
-            split_avoid_absolute_danger,
+            (split_avoid_absolute_danger),
 
             #1vn focus on survival, prefer easy and surely wayout
             cases([
-                no_cut_can_see_tail,
-                no_cut_can_reach_tail,
+                (no_cut_can_see_tail),
+                (no_cut_can_reach_tail),
             ]),
         ])(moves)
 
     def no_cut_can_see_tail(moves):
-        moves = [a for a in moves if 1==1
+        moves = [a for a in moves if 1==0
                  or no_cut_can_see_my_tail(a)
                  or no_cut_can_see_other_tail(a)
                  ]
@@ -574,7 +574,7 @@ def main(game_state):
             return moves
 
     def no_cut_can_reach_tail(moves):
-        moves = [a for a in moves if 1==1
+        moves = [a for a in moves if 1==0
                  or no_cut_can_reach_my_tail(a)
                  or no_cut_can_reach_other_tail(a)
                  ]
@@ -591,17 +591,17 @@ def main(game_state):
         return False
     
     def no_cut_can_see_my_tail(a):
-        if any([path_connected(a, snake.head) for snake in g.snakes]):
+        if any([path_connected(a, snake.head) for snake in g.others]):
             return False
         return path_connected(a, g.me.tail)
 
     def no_cut_can_see_other_tail(a):
-        if any([path_connected(a, snake.head) for snake in g.snakes]):
+        if any([path_connected(a, snake.head) for snake in g.others]):
             return False
         return any([path_connected(a, snake.tail) for snake in g.others])
 
     def no_cut_can_reach_my_tail(a):
-        if any([path_connected(a, snake.head) for snake in g.snakes]):
+        if any([path_connected(a, snake.head) for snake in g.others]):
             return False
         aset = path_connected_set(a)
         max_index = max([i for i,c in enumerate(g.me.body) if any([p in aset for p in adj_cells(c)]) ])
@@ -611,7 +611,7 @@ def main(game_state):
         return False
     
     def no_cut_can_reach_other_tail(a):
-        if any([path_connected(a, snake.head) for snake in g.snakes]):
+        if any([path_connected(a, snake.head) for snake in g.others]):
             return False
         aset = path_connected_set(a)
         adj_snakes = [snake for snake in g.others if any([p in aset for c in snake.body for p in adj_cells(c)])]
@@ -810,6 +810,7 @@ if __name__ == "__main__":
     log = {'id': '9dd43272-e5c3-482f-906d-e7b7f1734298', 'turn': 91, 'me': {'name': 'mark_snake_test BLUE', 'health': 100, 'body': [(8, 5), (8, 4), (9, 4), (10, 4), (10, 3), (9, 3), (8, 3), (8, 2), (9, 2), (10, 2), (10, 1), (9, 1), (9, 0), (9, 0)]}, 'others': [{'name': 'mark_snake', 'health': 96, 'body': [(2, 1), (2, 0), (1, 0), (0, 0), (0, 1), (1, 1), (1, 2), (1, 3), (1, 4), (1, 5), (2, 5), (3, 5), (4, 5), (5, 5)]}, {'name': 'mark_snake_test GREEN', 'health': 49, 'body': [(7, 8), (6, 8), (6, 9), (5, 9), (4, 9), (3, 9), (2, 9), (1, 9)]}, {'name': 'mark_snake_test RED', 'health': 98, 'body': [(9, 6), (9, 7), (9, 8), (9, 9), (8, 9)]}], 'food': [(7, 7)], 'module': 'simp', 'decision_path': ['1vn', 'split long', 'avoid confined: []'], 'next_coord': (9, 5), 'next_move': 'right', 'allowed_moves': [(9, 5), (7, 5), (8, 6)], 'time': '0.011s'}
     log = {'id': 'a25bff64-e6a2-4f30-a2f6-dfa7c4ff234f', 'turn': 69, 'me': {'name': 'mark_snake_test RED', 'health': 93, 'body': [(5, 6), (6, 6), (7, 6), (8, 6), (8, 7), (8, 8), (8, 9), (7, 9), (7, 8), (7, 7)]}, 'others': [{'name': 'mark_snake', 'health': 100, 'body': [(4, 5), (3, 5), (3, 4), (3, 3), (4, 3), (4, 3)]}, {'name': 'mark_snake_test GREEN', 'health': 69, 'body': [(4, 7), (5, 7), (5, 8), (5, 9), (4, 9), (3, 9), (2, 9)]}, {'name': 'mark_snake_test BLUE', 'health': 93, 'body': [(2, 7), (1, 7), (1, 8), (1, 9), (1, 10), (0, 10), (0, 9)]}], 'food': [(8, 10)], 'module': 'simp', 'decision_path': ['1vn', 'split length medium', 'avoid confined: []', 'try to go to open space'], 'next_coord': (4, 6), 'next_move': 'left', 'time': '0.007s'}
     log = {'id': '26140136-e6ac-45b7-abf9-4938e61cad78', 'turn': 176, 'me': {'name': 'mark_snake_test BLUE', 'health': 81, 'body': [(0, 6), (0, 5), (0, 4), (1, 4), (2, 4), (2, 3), (3, 3), (4, 3), (5, 3), (6, 3), (6, 4), (6, 5), (6, 6), (6, 7), (6, 8), (5, 8), (4, 8), (4, 7), (4, 6)]}, 'others': [{'name': 'mark_snake', 'health': 98, 'body': [(3, 7), (2, 7), (1, 7), (1, 8), (1, 9), (1, 10), (2, 10), (3, 10), (4, 10), (5, 10)]}, {'name': 'mark_snake_test GREEN', 'health': 98, 'body': [(10, 8), (10, 7), (9, 7), (8, 7), (7, 7), (7, 6), (7, 5), (7, 4), (8, 4), (9, 4), (10, 4), (10, 3), (10, 2), (10, 1), (10, 0), (9, 0), (8, 0), (7, 0), (6, 0), (5, 0), (4, 0), (3, 0), (3, 1)]}], 'food': [(3, 8)], 'module': 'simp', 'decision_path': ['1vn', 'split long', 'try to go to open space', 'go to open space (2, 6)'], 'next_coord': (1, 6), 'next_move': 'right', 'time': '0.004s'}
+    log = {'id': '4c1a77d8-5372-409c-94fa-bf7550c2245f', 'turn': 208, 'me': {'name': 'mark_snake_test BLUE', 'health': 80, 'body': [(10, 6), (9, 6), (8, 6), (7, 6), (6, 6), (5, 6), (5, 7), (5, 8), (6, 8), (7, 8), (8, 8), (8, 9), (9, 9)]}, 'others': [{'name': 'Frank The Tank', 'health': 76, 'body': [(3, 5), (3, 4), (2, 4), (1, 4), (1, 5), (0, 5), (0, 6), (0, 7), (0, 8), (0, 9), (1, 9), (2, 9), (2, 8), (1, 8), (1, 7), (1, 6)]}, {'name': 'Kakemonsteret-v2', 'health': 99, 'body': [(10, 0), (9, 0), (9, 1), (8, 1), (8, 0), (7, 0), (6, 0), (5, 0), (4, 0), (3, 0), (2, 0), (2, 1), (1, 1), (1, 0), (0, 0), (0, 1), (0, 2)]}], 'food': [(10, 5)], 'module': 'simp', 'decision_path': ['1vn', 'split long', 'split no cut can see tail'], 'next_coord': (10, 5), 'next_move': 'down', 'time': '0.005s'}
 
     game_state = init_from_log(log)
     main(game_state)
