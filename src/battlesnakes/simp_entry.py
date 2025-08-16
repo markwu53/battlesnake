f2c1c54773d0787bbca56e76de7da6743f7c0e9c
Mark Wu <mark.wu@encova.com>
2025-08-15 21:55:02 -0400

    update

--- src/battlesnakes/simp_entry.py
+++ src/battlesnakes/simp_entry.py
@@ -409,6 +409,7 @@ def main(game_state, log=True):
         def connected(a, b):
             return is_adjacent(a, b) or distance_vector_abs(a,b) == (1,1)
 
+        cut_set.sort()
         cut_set_adjacency = [(a, [b for b in cut_set if connected(a, b)]) for a in cut_set ]
         cut_set_adj_number = [(a, nb) for a,b in cut_set_adjacency for nb in [len(b)]]
         terminals = [(a,nb) for a,nb in cut_set_adj_number if nb == 1]
@@ -427,7 +428,9 @@ def main(game_state, log=True):
                 continue
             a = cut_set[i-1]
             b = [b for b in cut_set_copy if b not in cut_set[:i] and connected(a, b)]
-            if len(b) != 1: return False
+            if len(b) != 1: 
+                g.decision_path.append(f"anomaly cut_set {cut_set}")
+                return False
             b = take_first(b)
             cut_set[i] = b
 
