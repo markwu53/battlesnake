e6bca376aed17a23a9b37e6a46d29deb9175bb36
Mark Wu <mark.wu@encova.com>
2025-10-12 09:50:08 -0400

    update chase_other_tail remove direct path add enemy food lookout

--- src/battlesnakes/decision_flow.py
+++ src/battlesnakes/decision_flow.py
@@ -139,7 +139,9 @@ def main(game_state, log=True, log_db=False):
             #(cond(7 <= g.me.length < 10)(split_choice)),
             cond(g.me.length < 10)(split_choice),
             cond(g.me.length <= 10)(killer_near_prefer_away_border),
-            cond(len(g.others) == 1 and g.me.length > g.other.length)(chase_to_the_end),
+
+            #try disable this
+            #cond(len(g.others) == 1 and g.me.length > g.other.length)(chase_to_the_end),
 
             #these are effective in killing the only other
             #cond(len(g.others) == 1 and g.me.length > g.other.length)(par([longer_push, chase_other_tail])),
@@ -563,79 +565,86 @@ def main(game_state, log=True, log_db=False):
     def chase_other_tail(moves):
         if is_adjacent(g.me.head, g.other.tail):
             #don't follow too close
-            tail_move = [a for a in moves if path_connected(a, g.other.tail) and distance_pq(a, g.other.tail) == 2]
-            if len(tail_move) != 0:
-                g.decision_path.append("chase other tail detour")
-                return tail_move
-        elif distance_pq(g.me.head, g.other.tail) == 2:
-            tail_move = [a for a in moves if path_connected(a, g.other.tail) and distance_pq(a, g.other.tail) == 3]
-            if len(tail_move) != 0:
-                g.decision_path.append("chase other tail detour")
-                return tail_move
-        else:
-            """
-            if path_distance_pq(g.me.head, g.other.tail) > 8: return
-            tail_move = shortest_path_move(g.me.head, g.other.tail)
+            path_2 = grow_path(g.other.head, 2)[2]
+            if any([len([f for f in path if f in g.food]) >= 1 for path in path_2]):
+                tail_move = [a for a in moves if path_connected(a, g.other.tail) and distance_pq(a, g.other.tail) == 2]
+                if len(tail_move) != 0:
+                    g.decision_path.append("chase other tail detour")
+                    return tail_move
+
+        if path_distance_pq(g.me.head, g.other.tail) == 2:
+            path_3 = grow_path(g.other.head, 3)[3]
+            if any([len([f for f in path if f in g.food]) >= 2 for path in path_3]):
+
+                tail_move = [a for a in moves if path_connected(a, g.other.tail) and distance_pq(a, g.other.tail) == 3]
+                if len(tail_move) != 0:
+                    g.decision_path.append("chase other tail detour")
+                    return tail_move
+
+        """
+        if path_distance_pq(g.me.head, g.other.tail) > 8: return
+        tail_move = shortest_path_move(g.me.head, g.other.tail)
+        tail_move = [a for a in moves if a in tail_move]
+        if len(tail_move) != 0:
+            g.decision_path.append("chase other tail")
+            return tail_move
+        """
+
+
+        chasing_info = [(i,c,p, path_distance_pq(g.me.head, p), g.other.length-i-1) 
+                        for i,c in enumerate(g.other.body)
+                        if c != g.other.head
+                        #and path_distance_pq(g.me.head, c) == distance_pq(g.me.head, c) 
+                        for p in adj_cells(c) if p in g.me.territory
+                        ]
+        chasing_info = [info for info in chasing_info for i,c,p,d,t in [info] if abs(d-t) <= 1]
+        if len(chasing_info) == 0: return
+        chasing_info = prefer_by_score(lambda a: a[0])(chasing_info)
+        i, c, p, d, t = take_first(chasing_info)
+        tail_move = shortest_path_move(g.me.head, p)
+        moves = [a for a in moves if a in tail_move]
+        if len(moves) != 0:
+            g.decision_path.append(f"chase other tail via {c}")
+            return moves
+
+        """
+        chasing_info = [(i, c, path_distance_pq(g.me.head, c), g.other.length-i) 
+                        for i,c in enumerate(g.other.body) 
+                        if c != g.other.head
+                        and any([p in g.me.territory for p in adj_cells(c)])
+                        ]
+        chasing_info = [(*info, tail_length-d) for info in chasing_info for i,c,d,tail_length in [info]]
+        if len(chasing_info) == 0: return
+
+        #least meander, then closest to its own tail
+        chasing_info = prefer_by_rank(lambda a: a[4])(chasing_info)
+        chasing_info = prefer_by_score(lambda a: a[0])(chasing_info)
+        i, c, path_distance, tail_length, meander_length = take_first(chasing_info)
+
+        #for item in chasing_info: print(item)
+
+
+        #not enough room to meander
+        if tail_length * 1.2 > len(g.me.territory): return
+
+        if meander_length <= 0:
+            tail_move = shortest_path_move(g.me.head, c)
             tail_move = [a for a in moves if a in tail_move]
             if len(tail_move) != 0:
                 g.decision_path.append("chase other tail")
                 return tail_move
-            """
-
-            chasing_info = [(i,c,p, path_distance_pq(g.me.head, p), g.other.length-i-1) 
-                          for i,c in enumerate(g.other.body)
-                          if c != g.other.head
-                          and path_distance_pq(g.me.head, c) == distance_pq(g.me.head, c) 
-                          for p in adj_cells(c) if p in g.me.territory
-                          ]
-            chasing_info = [info for info in chasing_info for i,c,p,d,t in [info] if abs(d-t) <= 1]
-            if len(chasing_info) == 0: return
-            chasing_info = prefer_by_score(lambda a: a[0])(chasing_info)
-            i, c, p, d, t = take_first(chasing_info)
-            tail_move = shortest_path_move(g.me.head, p)
-            moves = [a for a in moves if a in tail_move]
-            if len(moves) != 0:
+        else:
+            #meander
+            direct_move = shortest_path_move(g.me.head, c)
+            meander_move = [a for a in moves if a not in direct_move and path_connected(a, c)]
+            if len(meander_move) != 0:
                 g.decision_path.append(f"chase other tail via {c}")
-                return moves
-
-            """
-            chasing_info = [(i, c, path_distance_pq(g.me.head, c), g.other.length-i) 
-                            for i,c in enumerate(g.other.body) 
-                            if c != g.other.head
-                            and any([p in g.me.territory for p in adj_cells(c)])
-                            ]
-            chasing_info = [(*info, tail_length-d) for info in chasing_info for i,c,d,tail_length in [info]]
-            if len(chasing_info) == 0: return
-
-            #least meander, then closest to its own tail
-            chasing_info = prefer_by_rank(lambda a: a[4])(chasing_info)
-            chasing_info = prefer_by_score(lambda a: a[0])(chasing_info)
-            i, c, path_distance, tail_length, meander_length = take_first(chasing_info)
-
-            #for item in chasing_info: print(item)
-
-
-            #not enough room to meander
-            if tail_length * 1.2 > len(g.me.territory): return
-
-            if meander_length <= 0:
-                tail_move = shortest_path_move(g.me.head, c)
-                tail_move = [a for a in moves if a in tail_move]
-                if len(tail_move) != 0:
-                    g.decision_path.append("chase other tail")
-                    return tail_move
-            else:
-                #meander
-                direct_move = shortest_path_move(g.me.head, c)
-                meander_move = [a for a in moves if a not in direct_move and path_connected(a, c)]
-                if len(meander_move) != 0:
-                    g.decision_path.append(f"chase other tail via {c}")
-                    return meander_move
-                direct_move = [a for a in moves if a in direct_move]
-                if len(direct_move) != 0:
-                    g.decision_path.append(f"chase other tail via {c}")
-                    return direct_move
-            """
+                return meander_move
+            direct_move = [a for a in moves if a in direct_move]
+            if len(direct_move) != 0:
+                g.decision_path.append(f"chase other tail via {c}")
+                return direct_move
+        """
 
     def corner_push(moves):
         snakes = [snake for snake in g.others if sum(distance_to_border(snake.head)) <= 1 and snake.length < g.me.length]
@@ -2897,6 +2906,7 @@ if __name__ == "__main__":
     log = {'id': 'd6641be4-2ae4-4324-bb22-2a94715a72ff', 'turn': 287, 'me': {'name': 'mark_snake', 'health': 94, 'length': 23, 'body': [(10, 5), (10, 4), (10, 3), (10, 2), (10, 1), (10, 0), (9, 0), (8, 0), (7, 0), (6, 0), (5, 0), (4, 0), (3, 0), (3, 1), (4, 1), (5, 1), (6, 1), (7, 1), (7, 2), (8, 2), (8, 3), (8, 4), (8, 5)], 'id': 'gs_tbBjYK6vfwrTvvTxHPPTYj8B'}, 'others': [{'name': 'slieks', 'health': 97, 'length': 22, 'body': [(5, 8), (5, 9), (5, 10), (4, 10), (4, 9), (3, 9), (2, 9), (1, 9), (1, 8), (1, 7), (0, 7), (0, 6), (1, 6), (1, 5), (1, 4), (1, 3), (2, 3), (2, 2), (3, 2), (4, 2), (4, 3), (4, 4)], 'id': 'gs_pbKxHXBbC7w8wDrwvR4gVrXD'}], 'food': [(0, 0), (8, 9), (1, 0)], 'module': 'decision_flow', 'decision_path': ['1v1', '1v1 longer push'], 'next_coord': (10, 6), 'next_move': 'up', 'time': '0.025s'}
     log = {'id': 'c61af5db-5f29-43ca-b636-ed511a896b18', 'turn': 200, 'me': {'name': 'mark_snake', 'health': 73, 'length': 18, 'body': [(10, 2), (9, 2), (8, 2), (7, 2), (6, 2), (5, 2), (4, 2), (4, 1), (4, 0), (3, 0), (2, 0), (2, 1), (2, 2), (2, 3), (2, 4), (2, 5), (1, 5), (0, 5)], 'id': 'gs_hjXFWY4V6GvRBmcBCRggcCkK'}, 'others': [{'name': 'go-st', 'health': 98, 'length': 15, 'body': [(8, 6), (8, 5), (8, 4), (8, 3), (7, 3), (6, 3), (5, 3), (5, 4), (4, 4), (4, 5), (4, 6), (4, 7), (4, 8), (3, 8), (2, 8)], 'id': 'gs_RGGHWxG46GYWVGwm3tSyBgxC'}], 'food': [(1, 2)], 'module': 'decision_flow', 'decision_path': ['1v1', 'chase other tail via (8, 3)'], 'next_coord': (10, 3), 'next_move': 'up', 'time': '0.011s'}
     log = {'id': '71fc9681-ef40-413b-aded-329cf9e8cfdb', 'turn': 185, 'me': {'name': 'mark_snake', 'health': 91, 'length': 15, 'body': [(6, 5), (6, 6), (6, 7), (5, 7), (5, 8), (4, 8), (4, 9), (3, 9), (2, 9), (2, 8), (3, 8), (3, 7), (2, 7), (1, 7), (1, 8)], 'id': 'gs_dcgHcV9xy4MmVcVQYpC3xCQC'}, 'others': [{'name': 'ich heisse marvin', 'health': 76, 'length': 13, 'body': [(9, 6), (9, 5), (8, 5), (8, 4), (8, 3), (8, 2), (7, 2), (6, 2), (5, 2), (4, 2), (3, 2), (3, 3), (3, 4)], 'id': 'gs_hhwjmmJc4mJQg7vbRBf9CDG9'}], 'food': [(3, 10), (7, 8), (5, 6)], 'module': 'decision_flow', 'decision_path': ['1v1', 'chase other tail', 'get food (5, 6)'], 'next_coord': (5, 5), 'next_move': 'left', 'time': '0.030s'}
+    log = {'id': 'e48a29e4-9aed-4f8b-964b-9100b90703ae', 'turn': 191, 'me': {'name': 'mark_snake', 'health': 95, 'length': 18, 'body': [(6, 3), (5, 3), (4, 3), (4, 2), (4, 1), (3, 1), (3, 0), (2, 0), (1, 0), (1, 1), (1, 2), (1, 3), (1, 4), (1, 5), (2, 5), (3, 5), (3, 6), (3, 7)], 'id': 'gs_TT8WVJbygSPFkSmWK9bg6G39'}, 'others': [{'name': 'soma-mini v1[standard]', 'health': 57, 'length': 7, 'body': [(5, 6), (4, 6), (4, 5), (4, 4), (5, 4), (6, 4), (6, 5)], 'id': 'gs_QPcH3PKmryPHFkhStJyjCJCW'}], 'food': [(2, 2), (6, 1), (5, 10), (2, 7)], 'module': 'decision_flow', 'decision_path': ['1v1', 'chase other tail detour', 'get food (6, 1)'], 'next_coord': (6, 2), 'next_move': 'down', 'time': '0.029s'}
 
 
 
