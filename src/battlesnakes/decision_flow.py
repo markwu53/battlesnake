54f2a5f4c2793baf5eaf2dd5d606579c57cc6bee
Mark Wu <mark.wu@encova.com>
2025-10-18 11:32:29 -0400

    add push territory before chase my tail

--- src/battlesnakes/decision_flow.py
+++ src/battlesnakes/decision_flow.py
@@ -117,6 +117,7 @@ def main(game_state, log=True, log_db=False):
             (wayout),
 
             cond(len(g.others) == 1 and g.me.length > g.other.length)(longer_push),
+            cond(len(g.others) == 1 and g.me.length > g.other.length)(longer_push_territory),
             cond(len(g.others) == 1 and g.me.length > g.other.length)(chase_my_tail),
 
             (cond(g.me.length > 8)(avoid_next_step_confinement)),
@@ -348,6 +349,14 @@ def main(game_state, log=True, log_db=False):
         if distance_pq(snake.head, snake2.head) != path_distance_pq(snake.head, snake2.head): return False
         return coming_to(snake, snake2.head) and coming_to(snake2, snake.head)
 
+    def longer_push_territory(moves):
+        if not path_connected(g.other.head, g.me.head): return
+        push_move = prefer_by_score(lambda a: len(new_territory(a)))(moves)
+        other_move = [a for a in moves if a not in push_move]
+        if len(other_move) != 0:
+            g.decision_path.append("1v1 longer push territory")
+            return push_move
+
     def longer_push(moves):
         #assume 1v1
         #if not coming_to_each_other(g.me, g.other): return
@@ -553,7 +562,7 @@ def main(game_state, log=True, log_db=False):
         chase_points = [info for info in chase_points for i,c,p,d,t in [info] if (d-t) <= 1]
         if len(chase_points) == 0: return
 
-        i,c,p,d,t = take_first(prefer_by_score(lambda a: abs(a[3]-a[4]))(chase_points))
+        i,c,p,d,t = take_first(prefer_by_score(lambda a: (a[3]-a[4]))(chase_points))
 
         if abs(d-t) <= 1:
             tail_move = shortest_path_move(g.me.head, p)
@@ -3095,6 +3104,7 @@ if __name__ == "__main__":
     log = {'id': '2cd55f62-27cc-4445-9157-4194b96372b7', 'turn': 163, 'me': {'name': 'mark_snake', 'health': 92, 'length': 17, 'body': [(3, 8), (4, 8), (4, 9), (4, 10), (5, 10), (6, 10), (6, 9), (6, 8), (6, 7), (6, 6), (6, 5), (6, 4), (5, 4), (4, 4), (4, 5), (3, 5), (2, 5)], 'id': 'gs_9G8BQPDmcSGKxyWtyCGt3HgS'}, 'others': [{'name': 'Frank The Tank', 'health': 96, 'length': 17, 'body': [(1, 8), (1, 7), (0, 7), (0, 6), (0, 5), (0, 4), (1, 4), (2, 4), (3, 4), (3, 3), (4, 3), (4, 2), (5, 2), (5, 1), (6, 1), (7, 1), (7, 2)], 'id': 'gs_trcqbwfj874KVtrwJhPvGWqJ'}], 'food': [(8, 6)], 'module': 'decision_flow', 'decision_path': ['1v1', 'avoid next step confinement [(3, 9)]'], 'next_coord': (2, 8), 'next_move': 'left', 'time': '0.010s'}
     log = {'id': 'a942118d-002e-4dd7-b602-c906a21dfa62', 'turn': 91, 'me': {'name': 'mark_snake', 'health': 50, 'length': 6, 'body': [(0, 5), (0, 6), (0, 7), (0, 8), (0, 9), (0, 10)], 'id': 'gs_hVYxPp63j8GJDYTHcxd4kfjd'}, 'others': [{'name': 'Copy of snake2_v3_FINAL_final(1)', 'health': 93, 'length': 13, 'body': [(5, 10), (4, 10), (3, 10), (2, 10), (2, 9), (3, 9), (4, 9), (4, 8), (3, 8), (3, 7), (4, 7), (5, 7), (6, 7)], 'id': 'gs_Y79MxMCY6wxdS8D74CVKBTtJ'}, {'name': 'ich heisse marvin', 'health': 62, 'length': 8, 'body': [(5, 4), (5, 5), (4, 5), (4, 6), (3, 6), (3, 5), (3, 4), (3, 3)], 'id': 'gs_myJMSFvWMkQQJJYYY6mpbKM6'}], 'food': [(0, 4), (1, 0), (6, 3), (7, 0), (9, 10)], 'module': 'decision_flow', 'decision_path': ['1vn', 'next to food'], 'next_coord': (0, 4), 'next_move': 'down', 'time': '0.048s'}
     log = {'id': '5e74779c-cbf5-419f-9dd7-d00f6ffa244e', 'turn': 46, 'me': {'name': 'mark_snake', 'health': 93, 'length': 9, 'body': [(4, 6), (3, 6), (2, 6), (1, 6), (1, 7), (1, 8), (1, 9), (1, 10), (2, 10)], 'id': 'gs_cY8f7CbPhfXpwBQ3rQFwkwbF'}, 'others': [{'name': 'SmartyRat', 'health': 84, 'length': 5, 'body': [(4, 8), (5, 8), (5, 7), (5, 6), (6, 6)], 'id': 'gs_cHB73yXm483PP9CDQQqrfhcV'}, {'name': '@~~~~@', 'health': 56, 'length': 4, 'body': [(7, 3), (6, 3), (6, 4), (7, 4)], 'id': 'gs_xK8bPbd7VGvJqTpJ8t7RKGgT'}, {'name': 'soma-mini v1[standard]', 'health': 56, 'length': 4, 'body': [(5, 1), (4, 1), (3, 1), (3, 2)], 'id': 'gs_t8yW9TDJSVxjTRCHT9Y4x7d4'}], 'food': [(9, 6), (7, 10)], 'module': 'decision_flow', 'decision_path': ['1vn', 'longer confront push'], 'next_coord': (4, 7), 'next_move': 'up', 'time': '0.037s'}
+    log = {'id': 'fe94ef9b-6200-4da0-a435-c05bb041864c', 'turn': 282, 'me': {'name': 'mark_snake', 'health': 89, 'length': 27, 'body': [(5, 7), (5, 6), (6, 6), (6, 5), (6, 4), (6, 3), (6, 2), (6, 1), (6, 0), (5, 0), (4, 0), (3, 0), (2, 0), (1, 0), (0, 0), (0, 1), (0, 2), (0, 3), (0, 4), (0, 5), (0, 6), (0, 7), (1, 7), (2, 7), (3, 7), (3, 8), (3, 9)], 'id': 'gs_6ffDrjddkdyMGGfHXSpgMTgb'}, 'others': [{'name': 'slieks', 'health': 98, 'length': 21, 'body': [(10, 6), (9, 6), (9, 5), (8, 5), (8, 4), (8, 3), (9, 3), (9, 2), (8, 2), (8, 1), (7, 1), (7, 2), (7, 3), (7, 4), (7, 5), (7, 6), (8, 6), (8, 7), (8, 8), (7, 8), (7, 7)], 'id': 'gs_vgXVJdTtGj9xcmMMMVbGgmcM'}], 'food': [(10, 0), (7, 10), (0, 8), (2, 10), (10, 1)], 'module': 'decision_flow', 'decision_path': ['1v1', 'chase my tail via body (5, 6) detour', 'get food (7, 10)'], 'next_coord': (5, 8), 'next_move': 'up', 'time': '0.037s'}
 
 
 
