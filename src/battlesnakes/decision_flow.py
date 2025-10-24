13d2f18657a88ff29a2581c13706095b17af0e29
Mark Wu <mark.wu@encova.com>
2025-10-24 09:34:02 -0400

    shorter_direct_connect try enclose more territory consider rect block sides

--- src/battlesnakes/decision_flow.py
+++ src/battlesnakes/decision_flow.py
@@ -441,11 +441,27 @@ def main(game_state, log=True, log_db=False):
         x1,y1 = target_terminal
         v1 = (x0,y1)
         v2 = (x1,y0)
+        v1_path = [(x0,y) for y in irange(y0, y1)] + [(x,y1) for x in irange(x0, x1)]
+        v2_path = [(x1,y) for y in irange(y0, y1)] + [(x,y0) for x in irange(x0, x1)]
+        v1_total_distance = sum([distance_pq(g.other.head, p) for p in v1_path])
+        v2_total_distance = sum([distance_pq(g.other.head, p) for p in v2_path])
+        if v1_total_distance < v2_total_distance:
+            preferred_v = v1
+        else:
+            preferred_v = v2
+        path_moves = shortest_path_move(g.me.head, preferred_v)
+        terminal_moves = [a for a in terminal_moves if a in path_moves]
+        if len(terminal_moves) != 0:
+            g.decision_path.append(f"move close to open space {target_terminal} via {preferred_v}")
+            return terminal_moves
+
+        """
         length_other_head_to_terminal = math.sqrt((x1 - g.other.head[0])**2 + (y1 - g.other.head[1])**2)
         length_other_head_to_v1 = math.sqrt((v1[0] - g.other.head[0])**2 + (v1[1] - g.other.head[1])**2)
         length_other_head_to_v2 = math.sqrt((v2[0] - g.other.head[0])**2 + (v2[1] - g.other.head[1])**2)
         length_terminal_to_v1 = math.sqrt((x1 - v1[0])**2 + (y1 - v1[1])**2)
         length_terminal_to_v2 = math.sqrt((x1 - v2[0])**2 + (y1 - v2[1])**2)
+
         if abs(length_other_head_to_v1 - (length_other_head_to_terminal + length_terminal_to_v1)) < 0.1:
             v2_moves = shortest_path_move(g.me.head, v2)
             terminal_moves = [a for a in terminal_moves if a in v2_moves]
@@ -476,6 +492,7 @@ def main(game_state, log=True, log_db=False):
                 if len(terminal_moves) != 0:
                     g.decision_path.append(f"move close to open space {target_terminal} via v2 prefer")
                     return terminal_moves
+        """
 
     def move_close_to_open_space(moves):
         killers = [snake for snake in g.others if snake.length > g.me.length and path_distance_pq(snake.head, g.me.head) <= 6]
@@ -3326,6 +3343,7 @@ if __name__ == "__main__":
     log = {'id': '64e83e38-7b34-473f-abfc-aaef98921c7b', 'turn': 178, 'me': {'name': 'mark_snake', 'health': 44, 'length': 15, 'body': [(3, 7), (3, 8), (3, 9), (4, 9), (5, 9), (5, 8), (6, 8), (6, 7), (5, 7), (4, 7), (4, 6), (5, 6), (6, 6), (6, 5), (5, 5)], 'id': 'gs_h6krYPySKCmCv78rkdxBSFMG'}, 'others': [{'name': 'SmartyRat', 'health': 93, 'length': 9, 'body': [(0, 10), (1, 10), (2, 10), (2, 9), (2, 8), (1, 8), (1, 7), (1, 6), (2, 6)], 'id': 'gs_MR4hGCrCGg9m48TKPq347cCR'}, {'name': 'go-st', 'health': 99, 'length': 15, 'body': [(6, 4), (7, 4), (7, 5), (8, 5), (9, 5), (10, 5), (10, 4), (10, 3), (10, 2), (10, 1), (10, 0), (9, 0), (9, 1), (8, 1), (8, 2)], 'id': 'gs_hrVx9HwkKDDrHVyKTjg8f7QW'}, {'name': 'soma-mini v1[standard]', 'health': 89, 'length': 12, 'body': [(5, 3), (5, 2), (5, 1), (4, 1), (4, 2), (3, 2), (2, 2), (2, 1), (3, 1), (3, 0), (4, 0), (5, 0)], 'id': 'gs_GQwdcScDc86Kbpt4j4TRpj8R'}], 'food': [(8, 0), (8, 9)], 'module': 'decision_flow', 'decision_path': ['1vn', "vulnerable snakes: [('SmartyRat', 1, (0, 9))]", 'attack vulnerables'], 'next_coord': (2, 7), 'next_move': 'left', 'time': '0.015s'}
     log = {'id': '690ec022-ff55-4b11-99a7-2ec4377b307f', 'turn': 164, 'me': {'name': 'mark_snake', 'health': 92, 'length': 11, 'body': [(6, 0), (6, 1), (6, 2), (7, 2), (8, 2), (9, 2), (9, 3), (9, 4), (9, 5), (9, 6), (8, 6)], 'id': 'gs_MSqcxBBwvKvc4wjcBvjG4kBQ'}, 'others': [{'name': 'CrystalSnake1', 'health': 66, 'length': 15, 'body': [(10, 4), (10, 5), (10, 6), (10, 7), (9, 7), (9, 8), (9, 9), (8, 9), (7, 9), (7, 8), (7, 7), (6, 7), (5, 7), (5, 8), (5, 9)], 'id': 'gs_xRhJJqkY4WSBmxTx36MtCVpc'}], 'food': [(9, 0), (6, 4)], 'module': 'decision_flow', 'decision_path': ['1v1', "vulnerable snakes: [('CrystalSnake1', 3, (10, 1))]", 'preliminary cut kill target: CrystalSnake1', 'go cut to (8, 1)'], 'next_coord': (7, 0), 'next_move': 'right', 'time': '0.007s'}
     log = {'id': 'e31d0b7d-362e-4846-99f4-e71dede70d2a', 'turn': 329, 'me': {'name': 'mark_snake', 'health': 95, 'length': 24, 'body': [(1, 0), (0, 0), (0, 1), (0, 2), (0, 3), (0, 4), (0, 5), (0, 6), (0, 7), (1, 7), (1, 8), (1, 9), (1, 10), (2, 10), (3, 10), (4, 10), (4, 9), (4, 8), (4, 7), (4, 6), (4, 5), (4, 4), (4, 3), (4, 2)], 'id': 'gs_cmHk7tCXmMkM3JVfXH3jqVh6'}, 'others': [{'name': 'Geriatric Jagwire', 'health': 91, 'length': 26, 'body': [(5, 10), (6, 10), (6, 9), (6, 8), (6, 7), (6, 6), (7, 6), (7, 7), (7, 8), (8, 8), (8, 7), (9, 7), (10, 7), (10, 6), (9, 6), (9, 5), (8, 5), (7, 5), (6, 5), (6, 4), (6, 3), (6, 2), (6, 1), (6, 0), (5, 0), (4, 0)], 'id': 'gs_6SYpg9tCjWfkrfFF9bfc6BTD'}], 'food': [(0, 10)], 'module': 'decision_flow', 'decision_path': ['1v1', "vulnerable snakes: [('Geriatric Jagwire', 4, (5, 6))]", "vulnerable but I'm short"], 'next_coord': (2, 0), 'next_move': 'right', 'time': '0.006s'}
+    log = {'id': '41190437-6e3c-4885-afdf-54a211042656', 'turn': 287, 'me': {'name': 'mark_snake', 'health': 46, 'length': 22, 'body': [(2, 3), (2, 2), (2, 1), (2, 0), (3, 0), (4, 0), (5, 0), (6, 0), (7, 0), (8, 0), (9, 0), (10, 0), (10, 1), (9, 1), (8, 1), (7, 1), (6, 1), (5, 1), (4, 1), (3, 1), (3, 2), (3, 3)], 'id': 'gs_JgpJkTXBgqcv9rTpbSQJFyWT'}, 'others': [{'name': 'Game of Chicken', 'health': 100, 'length': 26, 'body': [(1, 10), (2, 10), (2, 9), (3, 9), (4, 9), (4, 10), (5, 10), (6, 10), (7, 10), (8, 10), (8, 9), (8, 8), (8, 7), (8, 6), (9, 6), (9, 5), (9, 4), (9, 3), (10, 3), (10, 2), (9, 2), (8, 2), (7, 2), (6, 2), (6, 3), (6, 3)], 'id': 'gs_9KHmr7htMmdcwXhQRXJ8VMRB'}], 'food': [(1, 5), (7, 6)], 'module': 'decision_flow', 'decision_path': ['1v1', 'preliminary cut kill target: Game of Chicken', 'get food (1, 5)', 'move close to open space (7, 6) via v2 prefer'], 'next_coord': (2, 4), 'next_move': 'up', 'time': '0.033s'}
 
 
 
