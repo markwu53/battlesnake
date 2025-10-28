fca416f5b38b529f7f45b73b93f1b77b445d8172
Mark Wu <mark.wu@encova.com>
2025-10-28 17:49:56 -0400

    update

--- src/battlesnakes/test.py
+++ src/battlesnakes/test.py
@@ -4,21 +4,3 @@ b = random.choice(a)
 
 print(b)
 
-def sort(xs):
-    if len(xs) <= 1:
-        return xs
-    pivot = len(xs) // 2
-    return sort(xs[:pivot]) + [xs[pivot]] + sort(xs[pivot+1:])
-
-def sort(xs):
-    return xs if len(xs) <= 1 else [
-        sort(xs[:pivot]) + [xs[pivot]] + sort(xs[pivot+1:])
-        for pivot in [len(xs) // 2]
-    ][0]
-
-def sort(xs):
-    def sort2(length):
-        if length <= 1:
-            return xs
-        return sort(xs[:length//2]) + [xs[length//2]] + sort(xs[length//2+1:])
-    return sort2(len(xs))
